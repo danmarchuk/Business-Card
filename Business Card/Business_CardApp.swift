e309c766124e873a06538e637cc085c4d2996bc9
Danyil Marchuk <zemero58@gmail.com>
2023-01-16 15:22:56 +0100

    Initial Commit

---
+++ Business Card/Business_CardApp.swift
@@ -0,0 +1,17 @@
+//
+//  Business_CardApp.swift
+//  Business Card
+//
+//  Created by Данік on 16/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct Business_CardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
