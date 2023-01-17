0beca62336abac4f4c55a815ed89fb91751c1732
Danyil Marchuk <zemero58@gmail.com>
2023-01-17 16:25:21 +0100

    Completed App

--- Business Card/ContentView.swift
+++ Business Card/ContentView.swift
@@ -9,13 +9,42 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        ZStack {
+            LinearGradient(colors: [ Color(red: 0.21, green: 0.15, blue: 0.30),
+                                     Color(red: 0.10, green: 0.16, blue: 0.34) ],
+                           startPoint: .top, endPoint: .bottom)
+            .edgesIgnoringSafeArea(.all)
+            
+            VStack {
+                ZStack {
+                    Circle()
+                        .fill(.white)
+                        .frame(width: 200, height: 200)
+                    Image("ME")
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .frame(width: 200, height: 200)
+                        .clipShape(Circle())
+                        .overlay(
+                            Circle().stroke(Color.white, lineWidth: 5))
+                }
+                
+                Text("Danyil Marchuk")
+                    .font(Font.custom("Pacifico-Regular", size: 40))
+                    .foregroundColor(.white)
+                    .bold()
+                
+                Text("IOS Developer")
+                    .foregroundColor(.white)
+                    .bold().font(.system(size: 20))
+                Divider()
+                InfoView(text: "+48777888999", imageName: "phone.fill")
+                InfoView(text: "danmarchuk9@gmail.com", imageName: "envelope.fill")
+                
+            }
+            
+            
         }
-        .padding()
     }
 }
 
@@ -24,3 +53,5 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
+
+
---
+++ Business Card/InfoView.swift
@@ -0,0 +1,33 @@
+//
+//  InfoView.swift
+//  Business Card
+//
+//  Created by Данік on 17/01/2023.
+//
+
+import SwiftUI
+
+
+struct InfoView: View {
+    
+    let text: String
+    let imageName: String
+    
+    var body: some View {
+        RoundedRectangle(cornerRadius: 25)
+            .fill(Color.white)
+            .frame(height: 50)
+            .overlay( HStack {
+                Image(systemName: imageName)
+                    .foregroundColor(.green)
+                Text(text).foregroundColor(.black)
+            })
+            .padding(.all)
+    }
+}
+
+struct InfoView_Previews: PreviewProvider {
+    static var previews: some View {
+        InfoView(text: "Hello", imageName: "phone.fill").previewLayout(.sizeThatFits)
+    }
+}
